24574d736cf7e3fb8ecc33850261d01e6b764b40
Blay <godandblay@gmail.com>
2025-08-24 18:51:02 +0200

    worked on the home section and and finished the header

--- WEB SITE 1/script.js
+++ WEB SITE 1/script.js
@@ -1,11 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
+  //header section with all navigation
   let serachform = document.querySelector(".serach-form");
   document.querySelector("#search-btn").onclick = () => {
     serachform.classList.toggle("active");
+    shoppingcart.classList.remove("active");
+    Loginform.classList.remove("active");
+    navbar.classList.remove("active");
   };
 
   let shoppingcart = document.querySelector(".shopping-cart");
   document.querySelector("#cart-btn").onclick = () => {
     shoppingcart.classList.toggle("active");
+    searchform.classList.remove("active");
+    Loginform.classList.remove("active");
+    navbar.classList.remove("active");
   };
+  let Loginform = document.querySelector(".Login-form");
+  document.querySelector("#login-btn").onclick = () => {
+    Loginform.classList.toggle("active");
+    shoppingcart.classList.remove("active");
+    searchform.classList.remove("active");
+    navbar.classList.remove("active");
+  };
+  let navbar = document.querySelector(".navbar");
+  document.querySelector("#menu-btn").onclick = () => {
+    navbar.classList.toggle("active");
+    shoppingcart.classList.remove("active");
+    searchform.classList.remove("active");
+    Loginform.classList.remove("active");
+  };
+
+  window.onscroll = () => {
+    shoppingcart.classList.remove("active");
+    searchform.classList.remove("active");
+    Loginform.classList.remove("active");
+    navbar.classList.remove("active");
+  };
+  // header ends
 });
